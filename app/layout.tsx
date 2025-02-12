e9dc270c8617aee34be82c7dcee37e1fc71c97e0
rahul-lal10 <rahul.rawat@lal10.com>
2025-02-12 13:01:26 +0530

    ADD : tag added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,10 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { MainNav } from "@/components/layout/main-nav";
 import { GradientWrapper } from "@/components/layout/gradient-wrapper";
 import { Metadata } from "next";
+import Script from 'next/script';
+import { cn } from "@/lib/utils";
+import { Inter } from "next/font/google";
+
 const base64Data = 'iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAAsQAAALEBxi1JjQAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAGxSURBVFiD7ZY9axRRFIafM7ObxEIQbAQFG0tbwcrOTgQrK8HKH6CFYGMhCFZWFhYiVoKdhYWFhYWIhYiFhYUIQkAUDSisn7vvsbibZGZ3Z3Y2WRYL98DAzD3nPe95z9y5A3v8x6gkxXa7PQC0gGPAEPgKvHLODf/KhPu+fwZ4Adz8w+MZ8Ng5t5asgLUTwHvgYITpN+C8c+5HogLW2hLwETgcY74CnHXO/UxEwFr7GrieYAoL59zVnQpYa+8Cj3cQ4plz7u52BKy1V4CXCQcHGAGHnHO/4wSstVVgDJQTFvgEnHLONYILxSBba8+y8+AB6sBLa+1IGQSC4zhwP6XgAFeB58aYkhYIXsVpYwQ8KRQKDSEEgiulLPAEuJWBwA3P857KbQFr7WHgPVDLSOCbc+6EEALAWnsI+ABUM0rwzTl3TEgpJWCUYfAAVWPMYyGEwBhzH7ibg8BdIcQGnHMPgGc5JHhmjHm4+WCMuQU8zVjgiTHm9tYHY8wt4HGGAo+MMXe2G4wx14BHGQg8NMbcizIYY84BD1IUuG+MeRBnNMacAe6lIHDPGPMkzriH/4Y/pW+9JPQNnqsAAAAASUVORK5CYII=';
 
 export const metadata: Metadata = {
@@ -27,6 +31,11 @@ export const metadata: Metadata = {
   },
 };
 
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
 export default function RootLayout({
   children,
 }: {
@@ -34,8 +43,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <meta name="google-site-verification" content="7HALWNa8gEk_5k2_KUcQ_QxbEJh83FfWKsENOgI00m8" />
-      <body>
+      <head>
+        {/* Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-50440B50JK`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-50440B50JK');
+            `,
+          }}
+        />
+      </head>
+      <body className={cn("min-h-screen bg-background font-sans antialiased", inter.className)}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
